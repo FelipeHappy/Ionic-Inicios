22117032f0e41bd97658eb474e61ef1c7ab34a41
FelipeHappy <felipe.cristobal.vizcarra@gmail.com>
2021-09-21 13:42:26 -0300

    Inicio Ionic con bases de Angular

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
   constructor() {}
 
+  ngOnInit(){
+    console.log('Hola Mundo');
+    let numero = 20;
+    //Debugger: Herramienta para escanear el codigo, se utiliza en el navegador
+    debugger;
+    
+    numero = 15;
+
+    numero = numero + 30
+   
+    numero = 0;
+
+    console.log(numero)
+  }
 }
+ 
